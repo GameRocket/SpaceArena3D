ab20c5eb652eba140be2db79a1fdd1c260dfce52
rengetsu <rengetsu@mail.ru>
2022-10-02 23:02:52 +0300

    InputManager script updated

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // On Enable
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    // On Disable
+    private void OnDisable()
+    {
+        onFoot.Disable();
     }
 }
